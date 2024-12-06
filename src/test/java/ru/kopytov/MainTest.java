310437ccf092e169f568119247f772ee8521187b
Roman <glazinet1209@yandex.ru>
2024-12-06 14:18:12 +0300

    add module test

---
+++ src/test/java/ru/kopytov/MainTest.java
@@ -0,0 +1,14 @@
+package ru.kopytov;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainTest {
+    @Test
+    @DisplayName("Тест загрузки контекста")
+    void contextLoads(){
+
+    }
+}
